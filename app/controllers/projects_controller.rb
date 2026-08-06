a2e137522c326919cc1bd20ebabfd159c17c186d
Andrew DeLaRosa <delarosa1254@gmail.com>
2026-08-06 13:48:55 -0700

    Added Projects new and create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,6 +12,14 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = current_user.projects.new(project_params)
+
+    if @project.save
+      redirect_to projects_path, notice: "Project was successfully created."
+    else
+      Rails.logger.debug @project.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -22,4 +30,13 @@ class ProjectsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def project_params
+      params.require(:project).permit(
+        :title, 
+        :description, 
+        :due_date
+        )
+    end
 end
