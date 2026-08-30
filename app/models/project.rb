bf9ec298ac9ca9304e7482396a9feb2d28af6c98
Andrew DeLaRosa <delarosa1254@gmail.com>
2026-08-29 18:46:36 -0700

    Project progessive bars added

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,18 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
+
+  def total_tasks
+    tasks.count
+  end
+  
+  def completed_tasks
+      tasks.where(completed: true).count
+  end
+
+  def progress_percentage
+    return 0 if total_tasks.zero?
+
+    (completed_tasks.to_f / total_tasks.to_f * 100).round(2)
+  end
 end
