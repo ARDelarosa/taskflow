b69f9fd380bd4eb80baa11ad4c0ac7d50d631044
Andrew DeLaRosa <delarosa1254@gmail.com>
2026-09-02 23:58:46 -0700

    Partials project and task created

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,10 +2,14 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 500 }
+  validates :due_date, presence: true
+
   def total_tasks
     tasks.count
   end
-  
+
   def completed_tasks
       tasks.where(completed: true).count
   end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :details, length: { maximum: 500 }
+  validates :due_date, presence: true
 end
